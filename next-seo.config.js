1395e4f23ba815122769a44220feb2ee8a2f1d55
mina bessada <mina.adibe@gmail.com>
2022-10-12 04:24:15 +0200

    test seo

---
+++ next-seo.config.js
@@ -0,0 +1,35 @@
+const SEO = {
+  title: "Landing page | Test SEO | SEO landing page | On top",
+  description: "Landing page is required for each company to increase revenue",
+  canonical: "https://landing-page-beige-two.vercel.app/",
+  openGraph: {
+    url: "https://landing-page-beige-two.vercel.app/",
+    title: "Landing page | Test SEO | SEO landing page | On top",
+    description: "Landing page is required for each company to increase revenue",
+    images: [
+      ,
+      {
+        url: "https://landing-page-beige-two.vercel.app/bg1.jpg",
+        width: 800,
+        height: 600,
+        alt: "bg1",
+        type: "image/jpeg",
+      },
+      {
+        url: "https://landing-page-beige-two.vercel.app/bg2.jpg",
+        width: 900,
+        height: 800,
+        alt: "bg2",
+        type: "image/jpeg",
+      },
+    ],
+    site_name: "Home page",
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+};
+
+export default SEO;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import { StoreProvider } from "../utils/store";
-
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config";
 function MyApp({ Component, pageProps }) {
   return (
     <StoreProvider>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </StoreProvider>
   );
